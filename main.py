fba095c0a1355c207070778f7496b7f672ab4c27
the hottest goose around <e244572@edu.sbl.ch>
2022-01-18 15:35:22 +0000

    does this have to make sense

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello ya idiot")
+from bs4 import BeautifulSoup
+from requests import get
+
+url = "https://xkcd.com/archive/"
+res = get(url)
+
+soup = BeautifulSoup(res.content, "html.parser")
+
+hrefs = [elem['href'] for elem in soup.select(r"a[title]")]
+print(hrefs)
